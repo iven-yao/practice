9ed2c1a79bdab533a45a69d4c10fc842683381a8
ivenyao <ivenyao820109@gmail.com>
2023-03-04 14:45:41 -0800

    daily challenge mar 2023

---
+++ leetcode/q2444.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class q2444 {
+    public long countSubarrays(int[] nums, int minK, int maxK) {
+        long res = 0;
+            
+        int min=-1, max=-1, left = -1;
+        
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i]<minK || nums[i] > maxK) {
+                left = min = max = i;
+            } 
+            min = nums[i]==minK ? i : min;
+            max = nums[i]==maxK ? i : max;
+            res += Math.min(max, min)-left;  
+        }
+        return res;
+    }
+}
