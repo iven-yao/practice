5cb5e75477cc28c55b1d65fb8c5db6c8c90d6e18
ivenyao <ivenyao820109@gmail.com>
2023-03-12 00:38:23 -0800

    daily challenge mar 2023

---
+++ leetcode/q264.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class q264 {
+    public int nthUglyNumber(int n) {
+        int[] ugly = new int[n];
+
+        int index2 = 0, index3 = 0, index5 = 0;
+        int factor2 = 2, factor3 = 3, factor5 = 5;
+        ugly[0] = 1;
+        for(int i = 1; i < n; i ++) {
+            int min = Math.min(factor2, Math.min(factor3, factor5));
+            ugly[i] = min;
+
+            if(factor2 == min) factor2 = ugly[++index2]*2;
+            if(factor3 == min) factor3 = ugly[++index3]*3;
+            if(factor5 == min) factor5 = ugly[++index5]*5;
+        }
+
+        return ugly[n-1];
+    }
+}
