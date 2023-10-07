37af169ad740863861c21ab43bd0ef4ac1b74304
ivenyao <ivenyao820109@gmail.com>
2023-10-07 01:45:11 -0700

    #1 q58 Length of Last Word O(n)?

---
+++ top150/ArrayString/q58.java
@@ -0,0 +1,21 @@
+package top150.ArrayString;
+
+public class q58 {
+    public int lengthOfLastWord(String s) {
+        int len = 0;
+        char[] arr = s.toCharArray();
+
+        int i = arr.length-1;
+        while(i >= 0 && arr[i] == ' ') {
+            i--;
+        }
+
+        while(i >= 0 && arr[i] != ' ') {
+            len++;
+            i--;
+        }
+    
+
+        return len;
+    }
+}
