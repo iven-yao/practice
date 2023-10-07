bc06519edd58c16ee71dc7b0e6d520ece8f5b675
ivenyao <ivenyao820109@gmail.com>
2023-10-07 01:26:30 -0700

    #1 q12 Integer to Roman ??

---
+++ top150/ArrayString/q12.java
@@ -0,0 +1,18 @@
+package top150.ArrayString;
+
+public class q12 {
+    String[] romans = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+    int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+    public String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < values.length; i++) {
+            int val = values[i];
+            while(num >= val) {
+                sb.append(romans[i]);
+                num-=val;
+            }
+        }
+
+        return sb.toString();
+    }
+}
