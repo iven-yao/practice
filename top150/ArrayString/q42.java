dcd0fecbe7c6488eebf0db2dc797aabe148863bb
ivenyao <ivenyao820109@gmail.com>
2023-10-07 00:24:57 -0700

    #1 q42 Trapping Rain Water O(n)

---
+++ top150/ArrayString/q42.java
@@ -0,0 +1,28 @@
+package top150.ArrayString;
+
+public class q42 {
+    public int trap(int[] height) {
+        int n = height.length;
+        int[] front = new int[n];
+        int[] back = new int[n];
+
+        int currMax = 0;
+        for(int i = 0; i < n; i++) {
+            currMax = Math.max(currMax, height[i]);
+            front[i] = currMax;
+        }
+
+        currMax = 0;
+        for(int i = n-1; i>=0; i--) {
+            currMax = Math.max(currMax, height[i]);
+            back[i] = currMax;
+        }
+
+        int water = 0;
+        for(int i = 0; i < n; i++) {
+            water += Math.min(front[i],back[i]) - height[i];
+        }
+
+        return water;
+    }
+}
