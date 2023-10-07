b58d601f2e928414709be45d78ccefc537534a0d
ivenyao <ivenyao820109@gmail.com>
2023-10-07 12:47:42 -0700

    #1 q238 Product of Array Except Self, with O(1) space

--- top150/ArrayString/q238.java
+++ top150/ArrayString/q238.java
@@ -1,31 +1,55 @@
 package top150.ArrayString;
 
+import java.util.Arrays;
+
 public class q238 {
+    
+    // with O(1) space
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] front = new int[n];
-        int[] back = new int[n];
-
-        front[0] = nums[0];
-        for(int i = 1; i < n; i++) {
-            front[i] = front[i-1] * nums[i];
-        }
-
-        back[n-1] = nums[n-1];
-        for(int i = n-2; i>=0; i--) {
-            back[i] = back[i+1] * nums[i];
-        }
-
         int[] res = new int[n];
-        res[0] = back[1];
-        res[n-1] = front[n-2];
-        for(int i = 1; i < n-1; i++) {
-            res[i] = front[i-1]*back[i+1];
+        Arrays.fill(res, 1);
+        int front = 1;
+        for(int i = 0; i < n; i++) {
+            res[i] *= front;
+            front *= nums[i];
+        }
+
+        int back = 1;
+        for(int i = n-1; i >= 0; i--) {
+            res[i] *= back;
+            back *= nums[i];
         }
 
         return res;
-
     }
+
+    // with out division
+    // public int[] productExceptSelf(int[] nums) {
+    //     int n = nums.length;
+    //     int[] front = new int[n];
+    //     int[] back = new int[n];
+
+    //     front[0] = nums[0];
+    //     for(int i = 1; i < n; i++) {
+    //         front[i] = front[i-1] * nums[i];
+    //     }
+
+    //     back[n-1] = nums[n-1];
+    //     for(int i = n-2; i>=0; i--) {
+    //         back[i] = back[i+1] * nums[i];
+    //     }
+
+    //     int[] res = new int[n];
+    //     res[0] = back[1];
+    //     res[n-1] = front[n-2];
+    //     for(int i = 1; i < n-1; i++) {
+    //         res[i] = front[i-1]*back[i+1];
+    //     }
+
+    //     return res;
+
+    // }
 
     // shouldn't use division operation
     // public int[] productExceptSelf(int[] nums) {
