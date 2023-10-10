0f6847d68ecd5cbbab34a8496ab3f13f629809a6
ivenyao <ivenyao820109@gmail.com>
2023-10-09 23:49:05 -0700

    #2 q125. Valid Palindrome

---
+++ top150/TwoPointers/q125.java
@@ -0,0 +1,29 @@
+package top150.TwoPointers;
+
+public class q125 {
+    public boolean isPalindrome(String s) {
+        int head = 0;
+        int tail = s.length()-1;
+        s = s.toLowerCase();
+
+        while(head < tail) {
+            char chead = s.charAt(head);
+            char ctail = s.charAt(tail);
+            // System.out.println(chead + ","+ ctail);
+            if(!isAlphanumeric(chead)) head++;
+            else if(!isAlphanumeric(ctail)) tail--;
+            else if(chead == ctail) {
+                head++;
+                tail--;
+            } else return false;
+        }
+
+        return true;
+    }
+
+    private boolean isAlphanumeric(char c) {
+        if(c - 'a' >= 0 && c - 'a' < 26) return true;
+        if(c - '0' >= 0 && c - '0' < 10) return true;
+        return false;
+    }
+}
