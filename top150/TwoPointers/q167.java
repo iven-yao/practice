5539bd6da24f4de95650d38624c6605697b05bfd
ivenyao <ivenyao820109@gmail.com>
2023-10-10 00:03:26 -0700

    #2 q167. Two Sum II - Input Array Is Sorted

---
+++ top150/TwoPointers/q167.java
@@ -0,0 +1,18 @@
+package top150.TwoPointers;
+
+public class q167 {
+    public int[] twoSum(int[] numbers, int target) {
+        int head = 0;
+        int tail = numbers.length-1;
+
+        while(head < tail) {
+            int addup = numbers[head] + numbers[tail];
+
+            if(addup > target) tail--;
+            if(addup < target) head++;
+            if(addup == target) return new int[]{head+1, tail+1};
+        }
+
+        return new int[]{0,0};
+    }
+}
