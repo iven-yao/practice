bdd918fe552a9601c170646574547d068435538b
ivenyao <ivenyao820109@gmail.com>
2023-10-14 16:00:34 -0700

    q28 update

--- top150/ArrayString/q28.java
+++ top150/ArrayString/q28.java
@@ -1,21 +1,36 @@
 package top150.ArrayString;
 
 public class q28 {
+    // beat 100%
     public int strStr(String haystack, String needle) {
         // return haystack.indexOf(needle);
-        int ptr = 0;
-        for(int i = 0; i < haystack.length(); i++) {
-            if(haystack.charAt(i) == needle.charAt(ptr)) {
-                ptr++;
-                if(ptr == needle.length()) {
-                    return i-needle.length()+1;
+        int m = needle.length();
+        for(int i = 0; i <= haystack.length()-m; i++) {
+            if(haystack.charAt(i) == needle.charAt(0)) {
+                if(haystack.substring(i, i+m).equals(needle)) {
+                    return i;
                 }
-            } else {
-                i -= ptr;
-                ptr = 0;
             }
         }
 
         return -1;
     }
+
+    // public int strStr(String haystack, String needle) {
+    //     // return haystack.indexOf(needle);
+    //     int ptr = 0;
+    //     for(int i = 0; i < haystack.length(); i++) {
+    //         if(haystack.charAt(i) == needle.charAt(ptr)) {
+    //             ptr++;
+    //             if(ptr == needle.length()) {
+    //                 return i-needle.length()+1;
+    //             }
+    //         } else {
+    //             i -= ptr;
+    //             ptr = 0;
+    //         }
+    //     }
+
+    //     return -1;
+    // }
 }
