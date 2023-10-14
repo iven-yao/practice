9f9cacc21507f4b57b7707c65c12381a4337d67d
ivenyao <ivenyao820109@gmail.com>
2023-10-14 16:11:56 -0700

    #3 3. Longest Substring Without Repeating Characters

---
+++ top150/SlidingWindow/q3.java
@@ -0,0 +1,20 @@
+package top150.SlidingWindow;
+
+public class q3 {
+    public int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        int[] presentIndex = new int[128];
+        int head = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(presentIndex[c] > head) {
+                head = presentIndex[c];
+            }
+            presentIndex[c] = i+1;
+            max = Math.max(max, i-head+1);
+        }
+
+        return max;
+    }
+}
