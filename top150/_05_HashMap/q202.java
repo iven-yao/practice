35b56a4b723796e9316837141c75a87467d8d87c
ivenyao <ivenyao820109@gmail.com>
2023-10-15 04:13:12 -0700

    #3 202. Happy Number

---
+++ top150/_05_HashMap/q202.java
@@ -0,0 +1,25 @@
+package top150._05_HashMap;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class q202 {
+    public boolean isHappy(int n) {
+        Set<Integer> checked = new HashSet<>();
+
+        while(n != 1 && !checked.contains(n)) {
+            checked.add(n);
+            int sum = 0;
+
+            while(n != 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n = n/10;
+            }
+
+            n = sum;
+        }
+
+        return n == 1;
+    }
+}
