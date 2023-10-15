802918c2d1a4d215aeb43d205bd312f390e86e38
ivenyao <ivenyao820109@gmail.com>
2023-10-15 02:47:17 -0700

    #3 383. Ransom Note

---
+++ top150/_05_HashMap/q383.java
@@ -0,0 +1,17 @@
+package top150._05_HashMap;
+
+public class q383 {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+
+        for(char c: magazine.toCharArray()) {
+            count[c-'a']++;
+        }
+
+        for(char c: ransomNote.toCharArray()) {
+            if(count[c-'a']-- == 0) return false;
+        }
+
+        return true;
+    }
+}
