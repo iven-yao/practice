cce16895eeab9802bf3c82faeb28490215070a2c
ivenyao <ivenyao820109@gmail.com>
2023-10-15 02:15:54 -0700

    #3 73. Set Matrix Zeroes

---
+++ top150/_04_Matrix/q73.java
@@ -0,0 +1,59 @@
+package top150._04_Matrix;
+
+public class q73 {
+    // O(1)
+    public void setZeroes(int[][] matrix) {
+        boolean fr = false,fc = false;
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                if(matrix[i][j] == 0) {
+                    if(i == 0) fr = true;
+                    if(j == 0) fc = true;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        for(int i = 1; i < matrix.length; i++) {
+            for(int j = 1; j < matrix[0].length; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+            }}
+        }
+        if(fr) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if(fc) {
+            for(int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+    // O(m+n)
+    public void setZeroes_mn(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] cols = new int[n];
+        int[] rows = new int[m];
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(matrix[i][j] == 0) {
+                    cols[j] = 1;
+                    rows[i] = 1;
+                }
+            }
+        }
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(cols[j] == 1 || rows[i] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
