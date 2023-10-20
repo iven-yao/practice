8a80621a434dd2b51013cca6f12c8a2df444da96
YuHsuanLu <46243445+YuHsuanLu@users.noreply.github.com>
2023-10-20 14:16:58 -0700

    #3

--- top150/python_solution.py
+++ top150/python_solution.py
@@ -1,6 +1,7 @@
 import collections
 from collections import defaultdict
-
+import bisect
+from collections import deque
 # =============== Sliding Window ===============
 
 # 209. Miniize Size Subarray Sum
@@ -100,36 +101,29 @@ class Solution76:
 # =============== Matrix ===============
 # 36. Valid Sudoku
 class Solution:
-    def setZeroes(self, matrix):
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        first_row = -1
-        first_col = -1
-        m = len(matrix)
-        n = len(matrix[0])
-        for i in range(m):
-            if matrix[i][0] == 0:
-                first_col = 0
-        for j in range(n):
-            if matrix[0][j] == 0:
-                first_row = 0
-        for i in range(1, m):
-            for j in range(1, n):
-                if matrix[i][j] == 0:
-                    matrix[i][0] = 0
-                    matrix[0][j] = 0
-        for i in range(1, m):
-            for j in range(1, n):
-                if matrix[0][j] == 0 or matrix[i][0] == 0:
-                    matrix[i][j] = 0
+    def isValidSudoku(self, board):
+        n = len(board)
+        rows = [set() for _ in range(n)]
+        cols = [set() for _ in range(n)]
+        boxes = [[set() for _ in range(3)] for _ in range(3)]
+        for i in range(n):
+            for j in range(len(board)):
+                if board[i][j] != '.':
+                    if board[i][j] in rows[i]:
+                        return False
+                    else:
+                        rows[i].add(board[i][j])
+                    if board[i][j] in cols[j]:
+                        return False
+                    else:
+                        cols[j].add(board[i][j])
+                    
+                    if board[i][j] in boxes[i//3][j//3]:
+                        return False
+                    else:
+                        boxes[i//3][j//3].add(board[i][j])
 
-        if first_row == 0:
-            for j in range(n):
-                matrix[0][j] = 0
-        if first_col == 0:
-            for i in range(m):
-                matrix[i][0] = 0
+        return True
         
 # 54. Spiral Matrix
 class Solution54:
@@ -144,17 +138,22 @@ class Solution54:
         i = 0
         j = 0
         while len(result) < m*n:
+            # right
             for col in range(left, right+1):
                 result.append(matrix[top][col])
             top += 1
+            # down
             for row in range(top, down+1):
                 result.append(matrix[row][right])
             right -= 1
+            # check if we should stop if the edge is invalid
             if left > right or top > down:
                 break
+            # left
             for col in range(right, left-1, -1):
                 result.append(matrix[down][col])
             down -= 1
+            # up
             for row in range(down, top-1, -1):
                 result.append(matrix[row][left])
             left += 1
@@ -162,14 +161,18 @@ class Solution54:
     
 # 48. Rotate Image
 class Solution:
+    # T: O(M*N)
+    # S: O(1)
     def rotate(self, matrix):
         """
         Do not return anything, modify matrix in-place instead.
         """
         n = len(matrix[0])
+        # transpose
         for i in range(n):
             for j in range(i+1, n):  
                 matrix[i][j] , matrix[j][i] = matrix[j][i], matrix[i][j]
+        # mirror horizontally
         for i in range(n):
             matrix[i].reverse()
         
@@ -202,11 +205,6 @@ class Solution:
         for i in range(1, m):
             for j in range(1, n):
                 if matrix[i][0] == 0 or matrix[0][j] == 0:
-                    matrix[i][j] = 0
-        
-        for j in range(n):
-            if matrix[0][j] == 0:
-                for i in range(m):
                     matrix[i][j] = 0
         
         if first_col:
@@ -429,8 +427,12 @@ class Solution49:
         for s in strs:
             sorted_s = sorted(s)
             d[tuple(sorted_s)].append(s)
-        print(d.values())
-        return d.values()
+        result = []
+        for val in d.values():
+            result.append(val)
+        return result
+    
+    # Rabin-Karp Algorithm OAO?????
 
 
 class Solution48:
@@ -446,21 +448,27 @@ class Solution48:
             matrix[i].reverse()
 
 class Solution202:
+    # T: O(N) ~ O(logN)
+    # S: O(N) ~ O(logN)
     def isHappy(self, n):
         def get_next(n):
-            total=0
-            while n!=0:
+            total = 0
+            while n != 0:
                 digits = n%10
                 n = n // 10
                 total += digits * digits
             return total
 
         seen = set()
-        while n!=1 and n not in seen:
+        while n != 1 and n not in seen:
             seen.add(n)
             n = get_next(n)
 
-        return n==1
+        return n == 1
+
+    # Floyd’s Cycle Finding Algorithm
+    # T: O(N) or O(logN)
+    # S: O(1)
 
 class Solution219:
     def containsNearbyDuplicate(self, nums, k):
@@ -489,6 +497,360 @@ class Solution128:
             maxLength = max(maxLength, last - first + 1)  
             
         return maxLength
+
+# 228. Summary Ranges
+class Solution228:
+    # T: O(N)
+    # S: O(1)
+    def summaryRanges(self, nums):
+        n = len(nums)
+        result = []
+        if n == 0:
+            return result
+        l = 0
+        r = 0
+        while r < n:
+            while r+1 < n and nums[r]+1 == nums[r+1]:
+                r += 1
+            if l != r:
+                cur_res = str(nums[l]) + '->' + str(nums[r])
+                result.append(cur_res)
+            else:
+                result.append(str(nums[l]))
+            
+            r += 1
+            l = r
+        return result
+
+    # T: O(N)
+    # S: O(1)
+    def summaryRanges(self, nums):
+        n = len(nums)
+        result = []
+        if n == 0:
+            return result
+        minIdx = 0
+        maxIdx = 0
+
+        for r in range(1, n):
+            if nums[r] - nums[r-1] == 1:
+                maxIdx = r
+            else:
+                if minIdx == maxIdx:
+                    result.append("{}".format(nums[minIdx]))
+                else:
+                    result.append("{}->{}".format(nums[minIdx], nums[maxIdx]))
+                minIdx = r
+                maxIdx = r
+        # add the last minIndex and maxIndex
+        if minIdx == maxIdx:
+            result.append("{}".format(nums[minIdx]))
+        else:
+            result.append("{}->{}".format(nums[minIdx], nums[maxIdx]))
+        return result
+
+
+class Solution56():
+    # T: O(NlogN)
+    # S: O(N), bc sort
+    def merge(self, intervals):
+        result = []
+        intervals.sort(key=lambda x:x[0])
+        for s, e in intervals:
+            if not result or s > result[-1][1]:
+                result.append([s, e])
+            else:
+                result[-1][1] = max(result[-1][1], e)
+        return result
+
+# 57. Insert Interval    
+class Solution57():
+    # T: O(NlogN)
+    # S: O(N), bc sort
+    def insert(self, intervals, newInterval):
+        intervals.append(newInterval)
+        intervals.sort(key=lambda x:x[0])
+        result = []
+        for s, e in intervals:
+            if not result or s > result[-1][1]:
+                result.append([s, e])
+            else:
+                result[-1][1] = max(result[-1][1], e)
+        return result
     
-# sol = Solution49()
-# print(sol.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+    # T: O(N), use BS bc original intervals are sorted!
+    # S: O(N)
+    def insert(self, intervals, newInterval):
+        # .bisect_left()
+        starts = [s[0] for s in intervals]
+        idx = bisect.bisect_left(starts, newInterval[0])
+        # .insert()
+        intervals.insert(idx, newInterval)
+        result = []
+        for s, e in intervals:
+            if not result or s > result[-1][1]:
+                result.append([s, e])
+            else:
+                result[-1][1] = max(result[-1][1], e)
+        return result
+
+class Solution452:
+    def findMinArrowShots(self, points):
+        count = 1
+        n = len(points)
+        points.sort(key = lambda x:x[1])
+        cur_e = points[0][1]
+        for s, e in points:
+            if s <= cur_e:
+                continue
+            else:
+                count += 1
+                cur_e = e
+        return count
+
+class Solution2390:
+    def removeStars(self, s):
+        stack = []
+        for ch in s:
+            if ch != '*':
+                stack.append(ch)
+            else:
+                if stack:
+                    stack.pop()
+        return ''.join(stack)
+
+
+class Solution20():
+    def isValid(self, s):
+        d = {'(': ')', '[': ']', '{': '}'}
+        stack = []
+        for ch in s:
+            if ch in d.keys():
+                stack.append(d[ch])
+            else:
+                if not stack or ch != stack[-1]:
+                    return False
+                else:
+                    stack.pop()
+        return len(stack) == 0
+
+# 71. Simplify Path
+class Solution71():
+    # T: O(N)
+    # S: O(N)
+    def simplifyPath(self, path):
+        path_arr = path.split('/')
+        stack = []
+        for ch in path_arr:
+            if ch == '..':
+                if stack:
+                    stack.pop()
+            elif ch == '' or ch == '.':
+                continue
+            else:
+                stack.append(ch)
+        return '/' + '/'.join(stack)
+
+# 155. Min Stack
+class MinStack:
+    
+    def __init__(self):
+        self.s = []
+
+    # T: O(1)
+    def push(self, val):
+        if self.s:
+            min_val = min(self.s[-1][1], val)
+        else:
+            min_val = val
+        self.s.append((val, min_val))
+
+    # T: O(1)
+    def pop(self):
+        if self.s:
+            self.s.pop()
+
+    # T: O(1)
+    def top(self):
+        if self.s: 
+            return self.s[-1][0]
+
+    # T: O(1)
+    def getMin(self):
+        if self.s:
+            return self.s[-1][1]
+
+# 150. Evaluate Reverse Polish Notation
+class Solution150:
+    def evalRPN(self, tokens):
+        operations = ['+', '-', '*', '/']
+        stack = []
+        for ch in tokens:
+            if ch not in operations:
+                stack.append(int(ch))
+            else:
+                second = stack.pop()
+                first = stack.pop()
+                if ch == '+':
+                    stack.append(first + second)
+                if ch == '-':
+                    stack.append(first - second)
+                if ch == '*':
+                    stack.append(first * second)
+                if ch == '/':
+                    stack.append(int(first / second)) # truncate toward zero via change float into integer, works for positive and negative cases!
+        return stack.pop()
+
+class Solution:
+    def calculate(self, s):
+        num, sign, stack = 0, 1, [0]
+
+        for c in s:
+            if c.isdigit():
+                num = num*10 + int(c)
+            elif c==' ':
+                continue
+
+            elif c == '+':
+                stack[-1] += num * sign
+                sign = 1
+                num = 0
+            elif c == '-':
+                stack[-1] += num * sign
+                sign = -1
+                num = 0
+            elif c == '(':
+                stack.extend([sign,0])
+                sign = 1
+                num = 0
+            elif c == ')':
+                lastNum = (stack.pop() + num*sign) * stack.pop()
+                stack[-1] += lastNum
+                sign = 1
+                num = 0
+                
+        return stack[-1]+num*sign
+
+
+# ========== Greedy ==========
+class Solution649:
+    # T: O(N^3)
+    # S: O(1)
+    def predictPartyVictory(self, senate):
+        i = 0
+        while i < len(senate):
+            if senate[i] == 'R':
+                # .find(), T: O(N)
+                idx = senate.find('D')
+                if idx == -1:
+                    return 'Radiant'
+                # .replace(), only replace the first word we want to find
+                # O(N) for search, O(N) for replace -> O(N^2)
+                senate = senate.replace('R', '', 1)
+                senate = senate.replace('D', '', 1)
+                senate += 'R'
+            elif senate[i] == 'D':
+                idx = senate.find('R')
+                if idx == -1:
+                    return 'Dire'
+                senate = senate.replace('D', '', 1)
+                senate = senate.replace('R', '', 1)
+                senate += 'D'
+    # Use 2 queues
+    # T: O(N)
+    # S: O(N)
+    def predictPartyVictory(self, senate):
+        r_queue = deque()
+        d_queue = deque()
+        # In each queue, store the index of the character
+        for i, ch in enumerate(senate):
+            if ch == 'R':
+                r_queue.append(i)
+            elif ch == 'D':
+                d_queue.append(i)
+        idx = len(senate)
+        while r_queue and d_queue:
+            r_idx = r_queue.popleft()
+            d_idx = d_queue.popleft()
+            if r_idx < d_idx:
+                r_queue.append(idx)
+            else:
+                d_queue.append(idx)
+            idx += 1
+        return 'Radiant' if len(r_queue) > 0 else 'Dire'
+
+#
+class Solution:
+    def solve(self, board):
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        m = len(board)
+        n = len(board[0])
+        q = deque()
+        for i in range(m):
+            for j in [0, n-1]:
+                if board[i][j] == 'O':
+                    board[i][j] = 'B'
+                    q.append([i,j])
+        for i in [0, m-1]:
+            for j in range(1, n-1):
+                if board[i][j] == 'O':
+                    board[i][j] = 'B'
+                    q.append([i, j])
+        dirs = [(1,0), (-1,0), (0,1), (0,-1)]
+        while q:
+            x, y = q.popleft()
+            for dx, dy in dirs:
+                new_x = x + dx
+                new_y = y + dy
+                if 0 <= new_x < m and 0 <= new_y < n and board[new_x][new_y] == 'O':
+                    board[new_x][new_y] = 'B'
+                    q.append([new_x, new_y])
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'B':
+                    board[i][j] = 'O'
+                elif board[i][j] == 'O':
+                    board[i][j] = 'X'
+
+# =============== Binary Tree ===============
+
+# Definition for a binary tree node.
+class TreeNode: #  This class defines a simple binary tree node structure with three attributes:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution: # This class contains the method for finding the lowest common ancestor of two nodes in a binary tree.
+    # Recursive
+    def lowestCommonAncestor(self, root, p, q): # The method takes three arguments:
+        # recursive base case 1
+        if not root:
+            return None
+        # recursive base case 2
+        # start from the root, bc root is always the common ancestor of all nodes in the tree, although it might not be the "lowest" common ancestor
+        # if what we want to find: p or q is in the root -> we return the root because the root is the lowest common ancestor.
+        if root == p or root == q:
+            return root
+
+        left = right = None
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        
+        '''
+        If both left and right are not None, it means that p and q are found in different subtrees of the current root, making the current root the lowest common ancestor. 
+        In this case, the function returns the current root.
+        '''
+        if left and right:
+            return root
+        '''
+		either one of the chidren returned a node, meaning either p or q found on left or right branch.
+        Ex: assuming 'p' found in left child, right child returned original defeault 'None'. 
+		This means 'q' is somewhere below node where 'p' was found. Therefore, we dont need to search all the way, because node where 'p' found is LCA
+		'''
+        if left or right:
+            return left or right
+        
---
+++ top150/python_test.py
@@ -0,0 +1,79 @@
+
+from solution import *
+
+sol383 = Solution383()
+sol205 = Solution205()
+sol290 = Solution290()
+sol242 = Solution242()
+sol49 = Solution49()
+sol48 = Solution48()
+sol219 = Solution219()
+sol228 = Solution228()
+sol56 = Solution56()
+sol57 = Solution57()
+sol452 = Solution452()
+sol2390 = Solution2390()
+sol20 = Solution20()
+sol71 = Solution71()
+sol649 = Solution649()
+
+def test():
+    assert sol383.canConstruct('a', 'b') == False
+    assert sol383.canConstruct('aa', 'ab') == False
+    assert sol383.canConstruct('aa', 'aab') == True
+    assert sol205.isIsomorphic('egg', 'add') == True
+    assert sol205.isIsomorphic('foo', 'bar') == False
+    assert sol205.isIsomorphic('paper', 'title') == True
+    assert sol290.wordPattern('abba', 'dog cat cat dog') == True
+    assert sol290.wordPattern('abba', 'dog cat cat fish') == False
+    assert sol290.wordPattern('aaaa', 'dog cat cat dog') == False
+    assert sol242.isAnagram('anagram', 'nagaram') == True
+    assert sol242.isAnagram('rat', 'car') == False
+    assert sol219.containsNearbyDuplicate([1,2,3,1], 3) == True
+    assert sol219.containsNearbyDuplicate([1,0,1,1], 1) == True
+    assert sol219.containsNearbyDuplicate([1,2,3,1,2,3], 2) == False
+    assert sol49.groupAnagrams(["eat","tea","tan","ate","nat","bat"]) == [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
+    # # assert sol49.groupAnagrams(["eat","tea","tan","ate","nat","bat"]) == [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
+    assert sol49.groupAnagrams([""]) == [[""]]
+    assert sol49.groupAnagrams([]) == []
+    assert sol49.groupAnagrams(["a"]) == [["a"]]
+
+    assert sol228.summaryRanges([0,1,2,4,5,7]) == ["0->2","4->5","7"]
+    assert sol228.summaryRanges([0,2,3,4,6,8,9]) == ["0","2->4","6","8->9"]
+    assert sol56.merge([[1,3],[2,6],[8,10],[15,18]]) == [[1,6],[8,10],[15,18]]
+    assert sol56.merge([[1,4],[4,5]]) == [[1,5]]
+    assert sol57.insert([[1,3],[6,9]], [2,5]) == [[1,5],[6,9]]
+    assert sol57.insert([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8]) == [[1,2],[3,10],[12,16]]
+    assert sol452.findMinArrowShots([[10,16],[2,8],[1,6],[7,12]]) == 2
+    assert sol452.findMinArrowShots([[1,2],[3,4],[5,6],[7,8]]) == 4
+    assert sol452.findMinArrowShots([[1,2],[2,3],[3,4],[4,5]]) == 2
+    assert sol2390.removeStars("leet**cod*e") == "lecoe"
+    assert sol2390.removeStars("erase*****") == ""
+    assert sol20.isValid("()") == True
+    assert sol20.isValid("()[]{}") == True
+    assert sol20.isValid("(]") == False
+    assert sol71.simplifyPath("/home/") == "/home"
+    assert sol71.simplifyPath("/../") == "/"
+    assert sol71.simplifyPath("/home//foo/") == "/home/foo"
+
+    assert sol649.predictPartyVictory("RD") == "Radiant"
+    assert sol649.predictPartyVictory("RDD") == "Dire"
+    assert sol649.predictPartyVictory("RRDDD") == "Radiant"
+expected_matrix = [[7,4,1],[8,5,2],[9,6,3]]
+input_matrix = [[1,2,3],[4,5,6],[7,8,9]]
+def assert_test():
+    sol48.rotate(input_matrix)
+    assert input_matrix == [[7,4,1],[8,5,2],[9,6,3]]
+    # for i in range(len(expected_matrix)):
+    #     expected = expected_matrix[i]
+    #     # print(expected, '>>>>', input_matrix[i])
+    #     assert len(expected) == len(input_matrix[i])
+    #     assert all([a == b for a, b in zip(expected, input_matrix[i])])
+
+test()
+assert_test()
+
+assert expected_matrix == input_matrix
+assert [[7,4,1],[8,5,2],[9,6,3]] == [[7,4,1],[8,5,2],[9,6,3]]
+
+# sol48.rotate([[1,2,3],[4,5,6],[7,8,9]])
