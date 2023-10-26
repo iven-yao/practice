151e159f3e2eb864d5c99ff5bba447b390281500
iven-yao <chihkeny@usc.edu>
2023-10-26 14:35:54 -0700

    #6 100. Same Tree

---
+++ top150/_09_BinaryTreeGeneral/q100.java
@@ -0,0 +1,11 @@
+package top150._09_BinaryTreeGeneral;
+
+import node_definition.TreeNode;
+
+public class q100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
