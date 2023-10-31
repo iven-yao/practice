68a2aa39e0faf4bbd2e83548fa506b2096a30925
iven-yao <chihkeny@usc.edu>
2023-10-31 16:44:43 -0700

    play around async waterfall

--- nodejs/async_test.js
+++ nodejs/async_test.js
@@ -1,17 +1,24 @@
 var async = require('async');
 
 const waterfall = () => async.waterfall([
-	function fun1(done) {
-    console.log(done.toString().match(/function[^{]+\{([\s\S]*)\}$/)[1]);
-  	done(null, 'value1');
+  // first argument in callback function is error code, others are arguments for next function
+  // the 'last' argument in each function should be callback function
+  // the callback function doesn't have to be named exactly as callback, it could be anything, like done, like next etc.
+	function fun1(callback) {
+    console.log('inside of the callback function:')
+    console.log(callback.toString());
+  	callback(null, 'value1');
   },
-  function fun2(value1, done) {
+  function fun2(value1, callback) {
   	console.log(value1);
-    done(null, ['v','a','l','u','e','2']);
+    callback(null, ['v','a','l','u','e','2']);
   },
-  function fun3(value2, done) {
-  	console.log(value2);
-    done(null);
+  function fun3(value2, callback) {
+  	value2.forEach(e => console.log(e));
+    callback(null, 'value3', 'another_value3');
+  },
+  function fun4(value3, another_value3, callback) {
+    console.log(value3, another_value3);
   }
 ], (err) => {
 	if(err) console.log(err);
