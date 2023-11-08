571b401294e43fa535366aeb381c7449a669b05a
ivenyao <ivenyao820109@gmail.com>
2023-11-07 19:18:12 -0800

    #8 208. Implement Trie (Prefix Tree)

--- top150/_14_Trie/q208.java
+++ top150/_14_Trie/q208.java
@@ -1,12 +1,53 @@
 package top150._14_Trie;
 
 public class q208 {
-    class Node{ 
-        Node[] children = new Node[26];
-        boolean isEnd = false;
+    class Node {
+        private Node[] children;
+        private boolean isEndOfWord;
+    
+        public Node() {
+            children = new Node[26];
+            isEndOfWord = false;
+        }
+    
+        public void insert(String word, int index) {
+            if (index == word.length()) {
+                isEndOfWord = true;
+                return;
+            }
+    
+            char c = word.charAt(index);
+            if (children[c - 'a'] == null) {
+                children[c - 'a'] = new Node();
+            }
+    
+            children[c - 'a'].insert(word, index + 1);
+        }
+    
+        public boolean search(String word, int index) {
+            if (index == word.length()) {
+                return isEndOfWord;
+            }
+    
+            char c = word.charAt(index);
+            
+            Node child = children[c - 'a'];
+            return child != null && child.search(word, index + 1);
+        }
+    
+        public boolean startsWith(String prefix, int index) {
+            if( index == prefix.length()) return true;
+    
+            char c = prefix.charAt(index);
+            Node child = children[c-'a'];
+    
+            return child != null && child.startsWith(prefix, index+1);
+        }
     }
     
+    
     class Trie {
+        
         Node root;
     
         public Trie() {
@@ -14,34 +55,23 @@ public class q208 {
         }
     
         public void insert(String word) {
-            Node node = root;
-            for(char c: word.toCharArray()){
-                int index = c-'a';
-                if(node.children[index] == null) node.children[index] = new Node();
-                node = node.children[index];
-            }
-            node.isEnd = true;
+            root.insert(word, 0);
         }
         
         public boolean search(String word) {
-            Node node = root;
-            for(char c: word.toCharArray()){
-                int index = c-'a';
-                if(node.children[index] != null) node = node.children[index];
-                else return false;
-            }
-            return node.isEnd;
+            return root.search(word, 0);
         }
         
         public boolean startsWith(String prefix) {
-            Node node = root;
-            for(char c: prefix.toCharArray()){
-                int index = c-'a';
-                if(node.children[index] != null) node = node.children[index];
-                else return false;
-            }
-            return true;
+            return root.startsWith(prefix, 0);
         }
     }
     
+    /**
+     * Your Trie object will be instantiated and called as such:
+     * Trie obj = new Trie();
+     * obj.insert(word);
+     * boolean param_2 = obj.search(word);
+     * boolean param_3 = obj.startsWith(prefix);
+     */
 }
